593db4c318635fb4dc4e7192b8c45a7f72641434
Charles Doutriaux <doutriaux1@llnl.gov>
2017-05-05 14:16:26 -0700

    updated build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,17 @@
-export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC ${CFLAGS}"
-export CXXLAGS="${CFLAGS} ${CXXLAGS}"
-export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
-export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
+export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC"
+export CXXLAGS="${CFLAGS}"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
 export UVCDAT_ANONYMOUS_LOG=False
 
+if [ `uname` == Linux ]; then
+    # To make sure we get the correct g++
+    export LD_LIBRARY_PATH=${PREFIX}/lib:${LIBRARY_PATH}
+    export CC="gcc -Wl,-rpath=${PREFIX}/lib"
+    export CXX="g++ -Wl,-rpath=${PREFIX}/lib"
+else
+    export CC="gcc"
+    export CXX="g++"
+fi
 
 python setup.py install
